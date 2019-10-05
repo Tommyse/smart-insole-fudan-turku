8e5154e59602095a5c6b7420cd6ec212a326c450
dvillafruela <agazorsuroot@gmail.com>
2019-10-05 23:12:19 +0300

    Stepfiles uploader

--- web-app/django_project/urls.py
+++ web-app/django_project/urls.py
@@ -23,11 +23,11 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
     path('sessions/', user_views.sessions, name='sessions'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('steplab.urls')),
+    path('', include('users.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- web-app/steplab/views.py
+++ web-app/steplab/views.py
@@ -1,23 +1,24 @@
+import os
+
 from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .models import StepSession
 from .models import stepFile
-
+from django.core.files.storage import default_storage
 
 def _generateStepFiles(n):
     stepFiles = []
 
     for i in range(0, n):
-        stepFiles.append(stepFile("University File", "Diego", "43", "45as5df4", "500", None))
+        stepFiles.append(stepFile('University File', 'Diego', '43', '45as5df4', '500', None))
     
     return stepFiles
 
 @login_required(login_url='login')
 def home(request):
     n = 10
-    # (5 // 2) + (5%2!=0)
-
     stepFiles = _generateStepFiles(n)
     
 
@@ -31,8 +32,21 @@ def home(request):
     }
     return render(request, 'steplab/home.html', context) # request, template and context(arguments)
 
+
 @login_required(login_url='login')
 def recordings(request):
+    user = request.user
+    if request.method == 'POST':
+        #stepfiles = request.POST.getlist('new-stepfiles', None) # request.POST.get and request.POST.getlist
+        stepfiles = request.FILES.getlist('new-stepfiles')
+        print(stepfiles)
+        for stepfile in stepfiles:
+            usrFolder = user.username # user.email + user.id
+            full_filename = os.path.join(settings.MEDIA_ROOT, usrFolder, stepfile.name)
+            # TODO: SAVE THE FILE INTO DB FOR LINDA USERS
+            path = default_storage.save(full_filename, stepfile)
+            
+        print(stepfiles)
     stepFiles = _generateStepFiles(10)
 
     context = {
@@ -44,3 +58,9 @@ def recordings(request):
 
 def about(request):
     return render(request, 'steplab/about.html', {'title': 'About'})
+
+
+'''
+from django.contrib.auth.models import User
+users = User.objects.all()
+'''
